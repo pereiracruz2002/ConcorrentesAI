f8a148c11b4bfc5d1b5f6491e5f4786134f8447b
Flávio Pereira <pereiracruz2002@gmail.com>
2026-01-08 00:22:04 +0000

    adicionando o

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home(){
-    return <h1>Que 2026 seja o ano de sair da zona de conforto e fazer acontecer, transformando sonhos em realidade com ação e fé</h1>
+  return <h1>Que 2026 seja o ano de sair da zona de conforto e fazer acontecer, transformando sonhos em realidade com ação e fé</h1>
 }
 export default Home;

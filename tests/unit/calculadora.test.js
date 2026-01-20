29ca761c2a1e8817a1d9de382a8b5374de312872
Flávio Pereira <pereiracruz2002@gmail.com>
2026-01-20 00:26:33 +0000

    adicionando o banco com docker

---
+++ tests/unit/calculadora.test.js
@@ -0,0 +1,5 @@
+const calculadora = require("../../models/calculadora");
+test("somar 2+2 deveria retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+});
